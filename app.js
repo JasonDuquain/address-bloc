e72bbb032042ddfd5ccd0acf31f4f183b8abcc19
jasonduquain <jduquain@gmail.com>
2018-12-21 20:13:53 -0500

    checkpoint_prompt-and-menu: completed

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+const inquirer = require('inquirer');
+
+const MenuController = require('./controllers/MenuController');
+
+
+const menu = new MenuController();
+
+menu.clear();
+menu.main();
+
+
+/*
+
+const questions = [
+   {
+     type: "list",
+     name: "status",
+     message: "Are you hungry?: ",
+     choices: [
+       "Yes",
+       "No"
+     ]
+   }
+];
+
+inquirer.prompt(questions).then((answers) => {
+    if (answers.status === 'Yes') {
+        console.log('get up and eat!')
+    } else {
+        console.log("Get back to work!");
+    }
+}).catch((err) => {
+    console.log(err);
+});
+
+*/
+
+
+
+
+
